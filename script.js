61205c7ffe1b06f711df40f46f6d300a0f6922dd
chokhelimariami <chokhelimariami618@gmail.com>
2024-11-11 14:07:41 +0100

    js task 2

---
+++ script.js
@@ -0,0 +1,29 @@
+let number = 25;
+
+if (number * 25 > 100 ) { 
+    console.log(">100");
+    
+}else if( number*25 < 100) {
+    console.log("<100");
+}
+
+
+let number1 = 20;
+
+if (number1 > 50 && number1 <100) {
+    console.log("false");
+    
+} else {
+    console.log("true");
+    
+}
+
+let number2 = 5;
+
+if (number2 = 0 || number2< 0) {
+    console.log("true");
+    
+} else {
+    console.log("false");
+    
+}
